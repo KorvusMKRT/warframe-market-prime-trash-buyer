b7c748c2b3988301a225cc38e36921df6d2a46f9
KorvusMKRT <korvus.mkrt@gmail.com>
2023-05-20 20:56:02 +0500

    add quantity and platinum custom options

--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,20 @@ main();
 async function main() {
     program
         .option('-i, --items [items...]', 'specify item')
+        .option('-q, --quant [quantity...]', 'specify quantity of items')
+        .option('-p, --platinum [platinum...]', 'specify number of platinum')
     program.parse();
     let options = program.opts();
     if (options.items !== undefined && !Array.isArray(options.items)) {
         throw new Error(`Items is not an array`)
     };
-    let items = options.items ?? ITEM_NAMES_TO_BUY;
+    let customItems = options.items ?? ITEM_NAMES_TO_BUY;
     console.log('Options: ', options);
     console.log('Remaining arguments: ', program.args);
     let result = await axios.get<ItemsApiResponse>('https://api.warframe.market/v1/items');
     let allItems = result.data.payload.items
     let itemsToBuy = allItems.filter((item) => {
-        return items.includes(item.item_name);
+        return customItems.includes(item.item_name);
     });
     let allOrders: Array<Order> = [];
     for (const itemToBuy of itemsToBuy) {
@@ -35,25 +37,25 @@ async function main() {
         })
         allOrders = [...allOrders, ...resultOrders]
     }
+    let customQuantity = options.quant ?? 3;
+    let customPlatinum = options.platinum ?? 4;
     let coolOrders: Array<Order> = allOrders.filter((coolOrder) => {
-        return (coolOrder.platinum < 4 && coolOrder.quantity > 2 && coolOrder.user.status === 'ingame' && coolOrder.order_type === 'sell');
+        return (
+            coolOrder.quantity >= customQuantity &&
+            coolOrder.platinum <= customPlatinum &&
+            coolOrder.user.status === 'ingame' &&
+            coolOrder.order_type === 'sell'
+        );
     })
-
 
     let sortedOrders = coolOrders.sort((order1, order2) => {
         return order1.user.id.localeCompare(order2.user.id);
     });
 
     let messages = sortedOrders.map((order) => {
-        let sum = Math.min(order.platinum, 3);
+        let sum = Math.min(customPlatinum, order.platinum, 3);
         return `/w ${order.user.ingame_name} Hello! You have WTS order: ${order.item!.item_name} for ${order.platinum}. I would like to buy all ${order.quantity} for ${Math.min(3, order.platinum) * order.quantity} if you are interested :)`
     })
 
     console.log(messages)
-
-
-
-
-
-
 }
